1f0506d159dde4ef793d0c31577229d1a6b89393
Dave Mielke <dave@mielke.cc>
2015-11-20 09:40:58 -0500

    Use a common function to list the braille display models. (dm)

--- home/brltty-displays.php
+++ home/brltty-displays.php
@@ -3,195 +3,58 @@ If you discover that any other displays and/or models work,
 or if you know the technical details required to get another one to work,
 then please <A HREF="contact.html">let us know</A>.
 <DL>
-<DT>Albatross<DD>
-  <UL>
-  <LI>46/80
-  </UL>
-<DT>Alva<DD>
-  <UL>
-  <LI>ABT (3nn)
-  <LI>Delphi (4nn)
-  <LI>Satellite (5nn)
-  <LI>Braille System 40
-  <LI>Braille Controller 640/680
-  </UL>
-<DT>B2G<DD>
-<DT>Baum<DD>
-  <UL>
-  <LI>BrailleConnect 12/24/32/40/64/.80
-  <LI>Brailliant 24/32/40/64/.80
-  <LI>Conny 12
-  <LI>DM80 Plus
-  <LI>EcoVario 24/32/40/64/.80
-  <LI>Inka
-  <LI>PocketVario 24
-  <LI>Pronto! V3 18/40
-  <LI>Pronto! V4 18/40
-  <LI>RBT 40/80
-  <LI>Refreshabraille 18
-  <LI>SuperVario 24/32/40/64/.80
-  <LI>Vario 40/80
-  <LI>VarioConnect 12/24/32/40/64/.80
-  <LI>VarioPro 40/64/80
-  <LI>VarioUltra 20/32/40
-  </UL>
-<DT>BrailComm<DD>
-  <UL>
-  <LI>III
-  </UL>
-<DT>BrailleLite<DD>
-  <UL>
-  <LI>18/40/M20/M40
-  </UL>
-<DT>BrailleMemo<DD>
-  <UL>
-  <LI>Pocket (16)
-  <LI>Smart (16)
-  <LI>32
-  <LI>40
-  </UL>
-<DT>BrailleNote<DD>
-  <UL>
-  <LI>18/32
-  </UL>
-<DT>Braudi<DD>
-<DT>BrlAPI<DD>
-<DT>Cebra<DD>
-  <UL>
-  <LI>20/40/60/80/100/120/140
-  </UL>
-<DT>CombiBraille<DD>
-  <UL>
-  <LI>25/45/85
-  </UL>
-<DT>EcoBraille<DD>
-  <UL>
-  <LI>20/40/80
-  </UL>
-<DT>EuroBraille<DD>
-  <UL>
-  <LI>AzerBraille
-  <LI>Clio
-  <LI>Esys
-  <LI>Iris
-  <LI>NoteBraille
-  <LI>Scriba
-  </UL>
-<DT>FreedomScientific<DD>
-  <UL>
-  <LI>Focus 1 44/70/84
-  <LI>Focus 2 40/80
-  <LI>Focus Blue 14/40
-  <LI>PAC Mate 20/40
-  </UL>
-<DT>HandyTech<DD>
-  <UL>
-  <LI>Modular 20/40/80
-  <LI>Modular Evolution 64/88
-  <LI>Modular Connect 88
-  <LI>Active Braille
-  <LI>Active Star 40
-  <LI>Basic Braille 16/20/32/40/48/64/80
-  <LI>Braillino
-  <LI>Braille Wave
-  <LI>Easy Braille
-  <LI>Braille Star 40/80
-  <LI>Connect Braille 40
-  <LI>Bookworm
-  </UL>
-<DT>Hedo<DD>
-  <UL>
-  <LI>ProfiLine
-  <LI>MobiLine
-  </UL>
-<DT>HIMS<DD>
-  <UL>
-  <LI>Braille Sense
-  <LI>SyncBraille
-  <LI>Braille Edge
-  </UL>
-<DT>HumanWare<DD>
-  <UL>
-  <LI>Brailliant 32/40/80 bi
-  </UL>
-<DT>Iris<DD>
-<DT>Libbraille<DD>
-<DT>LogText<DD>
-  <UL>
-  <LI>32
-  </UL>
-<DT>MDV<DD>
-  <UL>
-  <LI>MB208
-  <LI>MB248
-  <LI>MB408L
-  <LI>MB408L+
-  <LI>Lilli Blu
-  </UL>
-<DT>Metec<DD>
-  <UL>
-  <LI>BD-40
-  </UL>
-<DT>MiniBraille<DD>
-  <UL>
-  <LI>20
-  </UL>
-<DT>MultiBraille<DD>
-  <UL>
-  <LI>MB125CR
-  <LI>MB145CR
-  <LI>MB185CR
-  </UL>
-<DT>NinePoint<DD>
-<DT>Papenmeier<DD>
-  <UL>
-  <LI>Compact 486
-  <LI>Compact/Tiny
-  <LI>IB 80 CR Soft
-  <LI>2D Lite (plus)
-  <LI>2D Screen Soft
-  <LI>EL 80
-  <LI>EL 2D 40/66/80
-  <LI>EL 40/66/70/80 S
-  <LI>EL 40/60/80 C
-  <LI>EL 2D 80 S
-  <LI>EL 40 P
-  <LI>EL 80 II
-  <LI>Elba 20/32
-  <LI>Trio 40/Elba20/Elba32
-  <LI>Live 20/40
-  </UL>
-<DT>Pegasus<DD>
-  <UL>
-  <LI>20/27/40/80
-  </UL>
-<DT>Seika<DD>
-  <UL>
-  <LI>3/4/5 (40)
-  <LI>80
-  <LI>Mini (16)
-  </UL>
-<DT>TechniBraille<DD>
-<DT>TSI<DD>
-  <UL>
-  <LI>Navigator 20/40/80
-  <LI>PowerBraille 40/65/80
-  </UL>
-<DT>TTY<DD>
-<DT>VideoBraille<DD>
-  <UL>
-  <LI>40
-  </UL>
-<DT>Virtual<DD>
-<DT>VisioBraille<DD>
-  <UL>
-  <LI>20/40
-  </UL>
-<DT>Voyager<DD>
-  <UL>
-  <LI>44/70
-  <LI>Part232 (serial adapter)
-  <LI>BraillePen/EasyLink
-  </UL>
-<DT>XWindow<DD>
+<?php
+   function list_models ($driver) {
+      echo("<DT>$driver<DD>\n");
+      $count = func_num_args();
+
+      if ($count > 1) {
+         echo("<UL>\n");
+            for ($index=1; $index<$count; $index+=1) {
+               $model = func_get_arg($index);
+               echo("<LI>$model</LI>\n");
+            }
+         echo("</UL>\n");
+      }
+   }
+
+   list_models("Albatross", "46/80");
+   list_models("Alva", "ABT (3nn)", "Delphi (4nn)", "Satellite (5nn)", "Braille System 40", "Braille Controller 640/680");
+   list_models("B2G");
+   list_models("Baum", "BrailleConnect 12/24/32/40/64/.80", "Brailliant 24/32/40/64/.80", "Conny 12", "DM80 Plus", "EcoVario 24/32/40/64/.80", "Inka", "PocketVario 24", "Pronto! V3 18/40", "Pronto! V4 18/40", "RBT 40/80", "Refreshabraille 18", "SuperVario 24/32/40/64/.80", "Vario 40/80", "VarioConnect 12/24/32/40/64/.80", "VarioPro 40/64/80", "VarioUltra 20/32/40");
+   list_models("BrailComm", "III");
+   LIST_MODELS("BRAILLELITE", "18/40/M20/M40");
+   list_models("BrailleMemo", "Pocket (16)", "Smart (16)", "32", "40");
+   list_models("BrailleNote", "18/32");
+   list_models("Braudi");
+   list_models("BrlAPI");
+   list_models("Cebra", "20/40/60/80/100/120/140");
+   list_models("CombiBraille", "25/45/85");
+   list_models("EcoBraille", "20/40/80");
+   list_models("EuroBraille", "AzerBraille", "Clio", "Esys", "Iris", "NoteBraille", "Scriba");
+   list_models("FreedomScientific", "Focus 1 44/70/84", "Focus 2 40/80", "Focus Blue 14/40", "PAC Mate 20/40");
+   list_models("HandyTech", "Modular 20/40/80", "Modular Evolution 64/88", "Modular Connect 88", "Active Braille", "Active Star 40", "Basic Braille 16/20/32/40/48/64/80", "Braillino", "Braille Wave", "Easy Braille", "Braille Star 40/80", "Connect Braille 40", "Bookworm");
+   list_models("Hedo", "ProfiLine", "MobiLine");
+   list_models("HIMS" , "Braille Sense" , "SyncBraille" , "Braille Edge");
+   list_models("HumanWare", "Brailliant 32/40/80 bi");
+   list_models("Iris");
+   list_models("Libbraille");
+   list_models("LogText", "32");
+   list_models("MDV", "MB208", "MB248", "MB408L", "MB408L+", "Lilli Blu");
+   list_models("Metec", "BD-40");
+   list_models("MiniBraille", "20");
+   list_models("MultiBraille", "MB125CR", "MB145CR", "MB185CR");
+   list_models("NinePoint");
+   list_models("Papenmeier", "Compact 486", "Compact/Tiny", "IB 80 CR Soft", "2D Lite (plus)", "2D Screen Soft", "EL 80", "EL 2D 40/66/80", "EL 40/66/70/80 S", "EL 40/60/80 C", "EL 2D 80 S", "EL 40 P", "EL 80 II", "Elba 20/32", "Trio 40/Elba20/Elba32", "Live 20/40");
+   list_models("Pegasus", "20/27/40/80");
+   list_models("Seika", "3/4/5 (40)", "80", "Mini (16)");
+   list_models("TechniBraille");
+   list_models("TSI", "Navigator 20/40/80", "PowerBraille 40/65/80");
+   list_models("TTY");
+   list_models("VideoBraille", "40");
+   list_models("Virtual");
+   list_models("VisioBraille", "20/40");
+   list_models("Voyager", "44/70", "Part232 (serial adapter)", "BraillePen/EasyLink");
+   list_models("XWindow");
+?>
 </DL>
