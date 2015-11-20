96b823570256e67dc99f50449a5b4c1117a289d9
Dave Mielke <dave@mielke.cc>
2015-11-19 21:55:30 -0500

    Update the braille display list. (dm)

--- home/brltty-displays.php
+++ home/brltty-displays.php
@@ -15,16 +15,25 @@ then please <A HREF="contact.html">let us know</A>.
   <LI>Braille System 40
   <LI>Braille Controller 640/680
   </UL>
+<DT>B2G<DD>
 <DT>Baum<DD>
   <UL>
+  <LI>BrailleConnect 12/24/32/40/64/.80
+  <LI>Brailliant 24/32/40/64/.80
+  <LI>Conny 12
+  <LI>DM80 Plus
+  <LI>EcoVario 24/32/40/64/.80
   <LI>Inka
-  <LI>Vario/RBT
-  <LI>SuperVario/Brailliant
-  <LI>PocketVario
-  <LI>VarioPro
-  <LI>EcoVario
-  <LI>VarioConnect/BrailleConnect
-  <LI>Refreshabraille
+  <LI>PocketVario 24
+  <LI>Pronto! V3 18/40
+  <LI>Pronto! V4 18/40
+  <LI>RBT 40/80
+  <LI>Refreshabraille 18
+  <LI>SuperVario 24/32/40/64/.80
+  <LI>Vario 40/80
+  <LI>VarioConnect 12/24/32/40/64/.80
+  <LI>VarioPro 40/64/80
+  <LI>VarioUltra 20/32/40
   </UL>
 <DT>BrailComm<DD>
   <UL>
@@ -34,9 +43,22 @@ then please <A HREF="contact.html">let us know</A>.
   <UL>
   <LI>18/40/M20/M40
   </UL>
+<DT>BrailleMemo<DD>
+  <UL>
+  <LI>Pocket (16)
+  <LI>Smart (16)
+  <LI>32
+  <LI>40
+  </UL>
 <DT>BrailleNote<DD>
   <UL>
   <LI>18/32
+  </UL>
+<DT>Braudi<DD>
+<DT>BrlAPI<DD>
+<DT>Cebra<DD>
+  <UL>
+  <LI>20/40/60/80/100/120/140
   </UL>
 <DT>CombiBraille<DD>
   <UL>
@@ -59,37 +81,52 @@ then please <A HREF="contact.html">let us know</A>.
   <UL>
   <LI>Focus 1 44/70/84
   <LI>Focus 2 40/80
-  <LI>Focus Blue 40
+  <LI>Focus Blue 14/40
   <LI>PAC Mate 20/40
   </UL>
 <DT>HandyTech<DD>
   <UL>
   <LI>Modular 20/40/80
   <LI>Modular Evolution 64/88
+  <LI>Modular Connect 88
   <LI>Active Braille
+  <LI>Active Star 40
+  <LI>Basic Braille 16/20/32/40/48/64/80
+  <LI>Braillino
   <LI>Braille Wave
   <LI>Easy Braille
   <LI>Braille Star 40/80
+  <LI>Connect Braille 40
   <LI>Bookworm
-  <LI>Braillino
+  </UL>
+<DT>Hedo<DD>
+  <UL>
+  <LI>ProfiLine
+  <LI>MobiLine
   </UL>
 <DT>HIMS<DD>
   <UL>
   <LI>Braille Sense
   <LI>SyncBraille
+  <LI>Braille Edge
   </UL>
 <DT>HumanWare<DD>
   <UL>
   <LI>Brailliant 32/40/80 bi
   </UL>
 <DT>Iris<DD>
+<DT>Libbraille<DD>
 <DT>LogText<DD>
   <UL>
   <LI>32
   </UL>
 <DT>MDV<DD>
   <UL>
-  <LI>MB208/MB408L/MB408S (protocol 5)
+  <LI>MB208
+  <LI>MB248
+  <LI>MB408L
+  <LI>MB408L+
+  <LI>Lilli Blu
   </UL>
 <DT>Metec<DD>
   <UL>
@@ -101,12 +138,11 @@ then please <A HREF="contact.html">let us know</A>.
   </UL>
 <DT>MultiBraille<DD>
   <UL>
-  <LI>MB125CR/MB145CR/MB185CR
+  <LI>MB125CR
+  <LI>MB145CR
+  <LI>MB185CR
   </UL>
 <DT>NinePoint<DD>
-  <UL>
-  <LI>Cebra 20/40/60/80/100/120/140
-  </UL>
 <DT>Papenmeier<DD>
   <UL>
   <LI>Compact 486
@@ -117,11 +153,13 @@ then please <A HREF="contact.html">let us know</A>.
   <LI>EL 80
   <LI>EL 2D 40/66/80
   <LI>EL 40/66/70/80 S
+  <LI>EL 40/60/80 C
   <LI>EL 2D 80 S
   <LI>EL 40 P
   <LI>EL 80 II
   <LI>Elba 20/32
   <LI>Trio 40/Elba20/Elba32
+  <LI>Live 20/40
   </UL>
 <DT>Pegasus<DD>
   <UL>
@@ -129,18 +167,22 @@ then please <A HREF="contact.html">let us know</A>.
   </UL>
 <DT>Seika<DD>
   <UL>
-  <LI>40
+  <LI>3/4/5 (40)
+  <LI>80
   <LI>Mini (16)
   </UL>
+<DT>TechniBraille<DD>
 <DT>TSI<DD>
   <UL>
   <LI>Navigator 20/40/80
   <LI>PowerBraille 40/65/80
   </UL>
+<DT>TTY<DD>
 <DT>VideoBraille<DD>
   <UL>
   <LI>40
   </UL>
+<DT>Virtual<DD>
 <DT>VisioBraille<DD>
   <UL>
   <LI>20/40
@@ -151,4 +193,5 @@ then please <A HREF="contact.html">let us know</A>.
   <LI>Part232 (serial adapter)
   <LI>BraillePen/EasyLink
   </UL>
+<DT>XWindow<DD>
 </DL>
