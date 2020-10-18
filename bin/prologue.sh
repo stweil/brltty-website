210b664f2475edeca756f063a95e0f0ed82cbe12
Dave Mielke <Dave@Mielke.cc>
2020-10-18 17:07:37 -0400

    Add a common shell prologue for the scripts. (dm)

---
+++ bin/prologue.sh
@@ -0,0 +1,25 @@
+. "`dirname "${0}"`/../source/brltty-prologue.sh"
+
+defaultWebsiteRoot="${programDirectory}/.."
+defaultSourceRoot="source"
+
+addOption_websiteRoot() {
+   addProgramOption w string.directory websiteRoot "top-level of website repository" "${defaultWebsiteRoot}"
+}
+
+checkOption_websiteRoot() {
+   [ -n "${websiteRoot}" ] || websiteRoot="${defaultWebsiteRoot}"
+   verifyInputDirectory "${websiteRoot}"
+   websiteRoot="$(resolveDirectory "${websiteRoot}")"
+}
+
+addOption_sourceRoot() {
+   addProgramOption s string.directory sourceRoot "top-level of source repository" "${defaultSourceRoot}"
+}
+
+checkOption_sourceRoot() {
+   [ -n "${sourceRoot}" ] || sourceRoot="${websiteRoot}/${defaultSourceRoot}"
+   verifyInputDirectory "${sourceRoot}"
+   [ "${sourceRoot#/}" != "${sourceRoot}" ] || sourceRoot="${websiteRoot}/${sourceRoot}"
+}
+
