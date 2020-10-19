36db46a49cf8abea00d2858cde58b632f33b1ad9
Dave Mielke <Dave@Mielke.cc>
2020-10-19 11:16:25 -0400

    Implement the chkdocs script. (dm)

--- bin/prologue.sh
+++ bin/prologue.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
+set -e
 . "`dirname "${0}"`/../source/brltty-prologue.sh"
 shopt -s nullglob
 
 defaultWebsiteRoot="${programDirectory}/.."
+defaultDocumentRoot="home/doc"
 defaultSourceRoot="source"
 
 addOption_websiteRoot() {
-   addProgramOption w string.directory websiteRoot "top-level of website repository" "${defaultWebsiteRoot}"
+   addProgramOption w string.directory websiteRoot "top-level of the website repository" "${defaultWebsiteRoot}"
 }
 
 checkOption_websiteRoot() {
@@ -15,13 +18,36 @@ checkOption_websiteRoot() {
    websiteRoot="$(resolveDirectory "${websiteRoot}")"
 }
 
+addOption_documentRoot() {
+   addProgramOption d string.directory documentRoot "top-level of the document hierarchy" "${defaultDocumentRoot}"
+}
+
+checkOption_documentRoot() {
+   [ -n "${documentRoot}" ] || documentRoot="${websiteRoot}/${defaultDocumentRoot}"
+   verifyInputDirectory "${documentRoot}"
+   [ "${documentRoot#/}" != "${documentRoot}" ] || documentRoot="${websiteRoot}/${documentRoot}"
+}
+
 addOption_sourceRoot() {
-   addProgramOption s string.directory sourceRoot "top-level of source repository" "${defaultSourceRoot}"
+   addProgramOption s string.directory sourceRoot "top-level of the source repository" "${defaultSourceRoot}"
 }
 
 checkOption_sourceRoot() {
    [ -n "${sourceRoot}" ] || sourceRoot="${websiteRoot}/${defaultSourceRoot}"
    verifyInputDirectory "${sourceRoot}"
    [ "${sourceRoot#/}" != "${sourceRoot}" ] || sourceRoot="${websiteRoot}/${sourceRoot}"
+}
+
+addOption_testMode() {
+   addProgramOption t flag testMode "Echo (rather than execute) the commands"
+}
+
+executeHostCommand() {
+   if "${testMode}"
+   then
+      programMessage "host command: ${*}"
+   else
+      "${@}"
+   fi
 }
 
