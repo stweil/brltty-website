dc5c0386388560ead8924c1206082d96106c92f5
Dave Mielke <Dave@Mielke.cc>
2020-12-23 08:03:19 -0500

    Update the documentation to 6.2.

--- home/brltty-parameters.php
+++ home/brltty-parameters.php
@@ -2,6 +2,6 @@
    define('BRLTTY_COPYRIGHT_HOLDER', 'The BRLTTY Developers');
    define('BRLTTY_COPYRIGHT_YEARS', '1995-2020');
 
-   define('BRLTTY_CURRENT_RELEASE', '6.1');
-   define('BRLAPI_CURRENT_RELEASE', '0.8.0');
+   define('BRLTTY_CURRENT_RELEASE', '6.2');
+   define('BRLAPI_CURRENT_RELEASE', '0.8.1');
 ?>
