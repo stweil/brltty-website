718e5a41daadcd0c5671b19f45ff4602e43b548a
Dave Mielke <Dave@Mielke.cc>
2021-01-28 08:15:34 -0500

    Release brltty 6.3. (dm)

--- home/brltty-parameters.php
+++ home/brltty-parameters.php
@@ -1,7 +1,7 @@
 <?php
    define('BRLTTY_COPYRIGHT_HOLDER', 'The BRLTTY Developers');
-   define('BRLTTY_COPYRIGHT_YEARS', '1995-2020');
+   define('BRLTTY_COPYRIGHT_YEARS', '1995-2021');
 
-   define('BRLTTY_CURRENT_RELEASE', '6.2');
-   define('BRLAPI_CURRENT_RELEASE', '0.8.1');
+   define('BRLTTY_CURRENT_RELEASE', '6.3');
+   define('BRLAPI_CURRENT_RELEASE', '0.8.2');
 ?>
