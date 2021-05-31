57997b1bc6c2e6ea5efc12e9c56a2e5c4270af3e
Dave Mielke <Dave@Mielke.cc>
2021-05-31 17:17:40 -0400

    Update the download documentation for BRLTTY and BrlAPI.

--- home/brltty-android-brlapi.php
+++ home/brltty-android-brlapi.php
@@ -1,9 +1,21 @@
 <p>
 The Java client library for BrlAPI on Android
-is within a Maven repository that we maintain.
+is stored within a Maven repository that we maintain.
 To use it, include the following code within
 your project's <code>build.gradle</code> script:
 </p>
+
+<?php
+   list_brlapi_file(
+      "brlapi-android.gradle",
+      "Groovy code for including the Android BrlAPI client library."
+   );
+?>
+
+<ul>
+<li>Your minimum SDK version must be at least <code>26</code>, which is Android 8 (Oreo).</li>
+<li>Remember to change the dependency version as needed.</li>
+</ul>
 
 <pre>
 repositories {
@@ -20,4 +32,40 @@ dependencies {
   )
 }
 </pre>
+
+You can also get BrlAPI's jar and libraries directly:
+
+<dl>
+   <?php
+      function list_brlapi_file ($name, $description) {
+         list_android_file("BrlAPI/$name", $description, $name);
+      }
+
+      function list_brlapi_library ($abi, $architecture, $name, $description) {
+         list_brlapi_file("$abi/$name", "The $architecture $description.");
+      }
+
+      function list_brlapi_libraries ($abi, $architecture) {
+         list_brlapi_library(
+            $abi, $architecture,
+            "libbrlapi.so", "C client library"
+         );
+
+         list_brlapi_library(
+            $abi, $architecture,
+            "libbrlapi_java.so", "JNI implementation"
+         );
+      }
+
+      list_brlapi_file(
+         "brlapi.jar",
+         "The jar containing BrlAPI's Java classes."
+      );
+
+      list_brlapi_libraries("armeabi-v7a", "arm 32-bit");
+      list_brlapi_libraries("arm64-v8a", "arm 64-bit");
+      list_brlapi_libraries("x86", "x86 32-bit");
+      list_brlapi_libraries("x86_64", "x86 64-bit");
+   ?>
+</dl>
 
--- home/brltty-android-sideload.php
+++ home/brltty-android-sideload.php
@@ -6,19 +6,24 @@ The differences between the two methods include:
 </p>
 
 <ul>
-  <li>
-  The Google Play version supports all of the mainstream architectures
-  (arm 32-bit, arm 64-bit, x86 32-bit, x86 64-bit)
-  whereas the sideload version only natively supports the arm 32-bit architecture.
-  It does work, albeit less efficiently, on the arm 64-bit architecture
-  because Android contains special support for this.
-  </li>
+   <li>
+   The Google Play version supports all of the mainstream architectures
+   (arm 32-bit, arm 64-bit, x86 32-bit, x86 64-bit)
+   whereas the sideload version only natively supports the arm 32-bit architecture.
+   It does work, albeit less efficiently, on the arm 64-bit architecture
+   because Android contains special support for this.
+   </li>
 
-  <li>
-  The Google Play version is a signed, release variant of the app
-  whereas the sideload version is an unsigned, debug variant of the app.
-  To install it, Android forces you to acknowledge that you might be using an untrusted source.
-  </li>
+   <li>
+   The Google Play version is a signed, release variant of the app
+   whereas the sideload version is an unsigned, debug variant of the app.
+   To install it, Android forces you to acknowledge that you might be using an untrusted source.
+   </li>
+
+   <li>
+   There's a delay between when we publish a new release on Google Play
+   whereas we can deploy a new sideload version immediately.
+   </li>
 </ul>
 
 <p>
