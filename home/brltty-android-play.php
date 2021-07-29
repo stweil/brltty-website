8263cdf5462fe46833ef71b0d04e90e35ef7073d
Dave Mielke <Dave@Mielke.cc>
2021-07-29 12:08:23 -0400

    Android: Document switching from the sideloaded version to the Google Play version.

--- home/brltty-android-play.php
+++ home/brltty-android-play.php
@@ -8,3 +8,12 @@ or go directly to its App page:
 <ul>
 <li><?php write_link("https://play.google.com/store/apps/details?id=org.a11y.brltty.android"); ?></li>
 </ul>
+
+<p>
+If BRLTTY has already been installed the old way, i.e. by having sideloaded it,
+then Google Play will neither automatically update it nor give you the option to reinstall it from there.
+This is because there's an incompatibility between the two versions -
+the Google Play version is a signed, release variant of the app
+whereas the sideload version is an unsigned, debug variant of the app.
+If you'd like to switch to the Google Play version then you'll need to first uninstall the sideload version.
+</p>
