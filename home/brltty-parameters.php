b03197faf07d08ba7b695748cb60840626040dbd
Dave Mielke <Dave@Mielke.cc>
2021-09-24 13:47:22 -0400

    Update the current release to 6.4 (from 6.3).

--- home/brltty-parameters.php
+++ home/brltty-parameters.php
@@ -2,6 +2,6 @@
    define('BRLTTY_COPYRIGHT_HOLDER', 'The BRLTTY Developers');
    define('BRLTTY_COPYRIGHT_YEARS', '1995-2021');
 
-   define('BRLTTY_CURRENT_RELEASE', '6.3');
-   define('BRLAPI_CURRENT_RELEASE', '0.8.2');
+   define('BRLTTY_CURRENT_RELEASE', '6.4');
+   define('BRLAPI_CURRENT_RELEASE', '0.8.3');
 ?>
