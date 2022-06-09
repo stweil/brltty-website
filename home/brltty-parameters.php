38a349e5d7267a9b791baf3609cba805ee8c038c
Dave Mielke <Dave@Mielke.cc>
2022-06-09 13:51:47 -0400

    Update the production release to 6.5 (from 6.4).

--- home/brltty-parameters.php
+++ home/brltty-parameters.php
@@ -1,7 +1,7 @@
 <?php
    define('BRLTTY_COPYRIGHT_HOLDER', 'The BRLTTY Developers');
-   define('BRLTTY_COPYRIGHT_YEARS', '1995-2021');
+   define('BRLTTY_COPYRIGHT_YEARS', '1995-2022');
 
-   define('BRLTTY_CURRENT_RELEASE', '6.4');
-   define('BRLAPI_CURRENT_RELEASE', '0.8.3');
+   define('BRLTTY_CURRENT_RELEASE', '6.5');
+   define('BRLAPI_CURRENT_RELEASE', '0.8.4');
 ?>
