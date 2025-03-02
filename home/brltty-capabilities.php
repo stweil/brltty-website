5a0cb5e83171ccce17f44767b0384b6994183ee2
Dave Mielke <Dave@Mielke.cc>
2025-03-02 17:31:59 -0500

    Remove items from the capabilities list that are so old that they are either confusing or not understood at all these days.

--- home/brltty-capabilities.php
+++ home/brltty-capabilities.php
@@ -1,7 +1,3 @@
-<B>BRLTTY</B> is just as effective and reliable as the <CODE>DOS</CODE> programs,
-perhaps even more so!
-It's definitely more flexible.
-<P>
 The <B>BRLTTY</B> daemon can be launched at system startup,
 thus it's always on to assist you when logging in,
 and allows the reading of console messages.
@@ -9,23 +5,7 @@ and allows the reading of console messages.
 <B>BRLTTY</B> allows full use of the virtual terminal feature of the Linux console driver.
 Blind users will discover that using several virtual terminals is very effective.
 <P>
-Since it interfaces directly with the console driver,
-<B>BRLTTY</B> allows you to use the scrollback buffer.
-This makes it possible, for example, to read all of the boot messages
-once <B>BRLTTY</B> has started up.
-<P>
-<B>BRLTTY</B> can be used in single user mode.
-<P>
-<B>BRLTTY</B> is small enough to fit comfortably on a root diskette.
-<P>
 <B>BRLTTY</B> can handle those video modes
 which offer more columns and/or rows than 80x25.
-<P>
-You can even use <A HREF="http://www.dosemu.org/">dosemu</A> with <B>BRLTTY</B>.
-The only restrictions are that
-cursor routing,
-arrow key simulation,
-and cut &amp; paste
-don't work (yet) when using its raw keyboard mode.
 <P>
 <B>BRLTTY</B> is adequate for performing system administration tasks.
