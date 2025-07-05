c97ce5dcc550f78c7ccf5a2baa7ae67072df9a05
Dave Mielke <Dave@Mielke.cc>
2025-07-05 16:27:33 -0400

    Update the current release to 6.8 (was 6.7).

--- home/brltty-parameters.php
+++ home/brltty-parameters.php
@@ -1,7 +1,7 @@
 <?php
    define('BRLTTY_COPYRIGHT_HOLDER', 'The BRLTTY Developers');
-   define('BRLTTY_COPYRIGHT_YEARS', '1995-2024');
+   define('BRLTTY_COPYRIGHT_YEARS', '1995-2025');
 
-   define('BRLTTY_CURRENT_RELEASE', '6.7');
-   define('BRLAPI_CURRENT_RELEASE', '0.8.6');
+   define('BRLTTY_CURRENT_RELEASE', '6.8');
+   define('BRLAPI_CURRENT_RELEASE', '0.8.7');
 ?>
